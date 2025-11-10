79554a68f2598ee68d7f880c53ef4cf093b81670
JUN_GYU\aplik <devjun1224@esjee.co.kr>
2025-11-10 23:00:33 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from financial-analyst!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tools.py
@@ -0,0 +1,50 @@
+import dotenv
+
+dotenv.load_dotenv()
+import re
+import os
+from firecrawl import FirecrawlApp, ScrapeOptions
+
+
+def web_search_tool(query: str):
+    """
+    Web Search Tool.
+    Args:
+        query: str
+            The query to search the web for.
+    Returns
+        A list of search results with the website content in Markdown format.
+    """
+    app = FirecrawlApp(api_key=os.getenv("FIRECRAWL_API_KEY"))
+
+    response = app.search(
+        query=query,
+        limit=5,
+        scrape_options=ScrapeOptions(
+            formats=["markdown"],
+        ),
+    )
+
+    if not response.success:
+        return "Error using tool."
+
+    cleaned_chunks = []
+
+    for result in response.data:
+
+        title = result["title"]
+        url = result["url"]
+        markdown = result["markdown"]
+
+        cleaned = re.sub(r"\\+|\n+", "", markdown).strip()
+        cleaned = re.sub(r"\[[^\]]+\]\([^\)]+\)|https?://[^\s]+", "", cleaned)
+
+        cleaned_result = {
+            "title": title,
+            "url": url,
+            "markdown": cleaned,
+        }
+
+        cleaned_chunks.append(cleaned_result)
+
+    return cleaned_chunks
