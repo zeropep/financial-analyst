959183f4b5eaf7531c4160828a86c628d361e8e0
JUN_GYU\aplik <devjun1224@esjee.co.kr>
2025-11-11 22:30:04 +0900

    tools 추가 및 sub_agent추가가

--- financial_advisor/agent.py
+++ financial_advisor/agent.py
@@ -3,10 +3,30 @@ from google.adk.models.lite_llm import LiteLlm
 
 MODEL = LiteLlm("openai/gpt-4o")
 
+def get_weather(city: str):
+    return f"The weather in {city} is 30 degrees."
+
+def convert_units(degrees: int):
+    return f"That is 40 farenheit."
+
+geo_agent = Agent(
+    name="GeoAgent",
+    instruction="You help with geo questions.",
+    model=MODEL,
+    description="Transfer to this agent when you have a geo related question."
+)
+
 agent = Agent(
     name="WeatherAgent",
     instruction="You help the user with weather related questions.",
     model=MODEL,
+    tools=[
+        get_weather,
+        convert_units,
+    ],
+    sub_agents=[
+        geo_agent,
+    ]
 )
 
 # "root_agent" is mandatory
